b96bed65b3ea3a0a54d5cd4fedf9fef8c79d2759
Rikuto <orikutobasuke@gmail.com>
2023-04-07 16:17:59 +0900

    nextscss

--- src/pages/samples/02/components/main/components/Skill.tsx
+++ src/pages/samples/02/components/main/components/Skill.tsx
@@ -1,4 +1,4 @@
-
+import { Icon } from '@chakra-ui/react'
 import style from './skill.module.scss'
 import {
     SiHtml5,
@@ -67,28 +67,42 @@ export function Skill() {
 
     return (
         <>
-            <section>
-                <div className={style.skillwrap}>
+            <div className={style.skillwrap}>
+                <div className={style.skillbox}>
                     <span>-Skill-</span>
                     <h2>What I use & am learning</h2>
                     <div>
                         <h3>Main technology used</h3>
-                        <div>
+                        <div className={style.iconbox}>
                             {iconArray.useicon.map((item, idx) =>
-                                <div key={idx}>
-
+                                <div key={idx} >
+                                    <Icon as={item} />
                                 </div>
                             )}
                         </div>
                     </div>
                     <div>
                         <h3>Technology you are learning (want to learn)</h3>
+                        <div className={style.iconbox}>
+                            {iconArray.studyicon.map((item, idx) =>
+                                <div key={idx}>
+                                    <Icon as={item} />
+                                </div>
+                            )}
+                        </div>
                     </div>
                     <div>
                         <h3>Tools used</h3>
+                        <div className={style.iconbox}>
+                            {iconArray.usingicon.map((item, idx) =>
+                                <div key={idx}>
+                                    <Icon as={item} />
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </div>
-            </section>
+            </div>
         </>
     )
 }
