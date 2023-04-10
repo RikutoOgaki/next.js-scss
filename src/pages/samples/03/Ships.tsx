88f135129af568e8f2257ae83b1f4b542e45d634
Rikuto <orikutobasuke@gmail.com>
2023-04-10 09:31:38 +0900

    nextscss

--- src/pages/samples/03/Ships.tsx
+++
@@ -1,6 +0,0 @@
-import { motion } from "framer-motion-3d";
-import { MotionConfig } from "framer-motion";
-import { useRef, useLayoutEffect } from "react";
-import { transition } from "./setting";
-import { Canvas, useThree } from "@react-three/fiber";
-import { Transform } from './Transform'
