22b2d0bc8d903a714f065e3dc6e63ee10b2fcafe
Rikuto <orikutobasuke@gmail.com>
2023-04-10 14:20:06 +0900

    nextscss

--- src/pages/samples/03/components/App.tsx
+++ src/pages/samples/03/components/App.tsx
@@ -2,6 +2,8 @@ import * as THREE from 'three'
 import * as REACT from 'react'
 import { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
+import { Mesh, Vector3 } from 'three'
+import style from './app.module.css'
 
 function Box(props: JSX.IntrinsicElements['mesh']) {
 
@@ -9,30 +11,41 @@ function Box(props: JSX.IntrinsicElements['mesh']) {
     const [hovered, hover] = useState(false)
     const [clicked, click] = useState(false)
 
-    useFrame((state, delte) => (ref.current.rotation.x += .01))
+
+    useFrame((state, delte) => (ref.current.rotation.y += .02))
+    useFrame((state, delte) => (ref.current.rotation.x += .02))
 
     return (
         <mesh
             {...props}
             ref={ref}
-            scale={clicked ? 1.5 : 1}
+            scale={hovered ? 1.5 : 1}
             onClick={(e) => click(!clicked)}
             onPointerOver={(e) => hover(true)}
             onPointerOut={(e) => hover(false)}>
             <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+            <meshStandardMaterial color={clicked ? 'hotpink' : 'blue'} />
         </mesh>
     )
 }
 
 export default function App() {
+    const Rig = ({ v = new Vector3() }) => {
+        return useFrame((state) => {
+            state.camera.position.lerp(v.set(state.mouse.x / 2, state.mouse.y / 2, 10), .09)
+        })
+    }
     return (
-        <Canvas>
-            <ambientLight intensity={.5} />
-            <spotLight position={[10, 10, 10]} angle={.15} penumbra={1} />
-            <pointLight position={[-10, -10, -10]} />
-            <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} />
-        </Canvas>
+        <div className={style.box}>
+            <Canvas>
+                <ambientLight intensity={.5} />
+                <Rig />
+                <spotLight position={[10, 10, 10]} angle={.15} penumbra={1} />
+                <pointLight position={[-10, -10, -10]} />
+                <Box position={[-1.2, 0, 0]} />
+                <Box position={[1.2, 0, 0]} />
+                <Box position={[0, 0, -10]} />
+            </Canvas>
+        </div>
     )
 }
