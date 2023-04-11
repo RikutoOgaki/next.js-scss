ba30e0ea68eb87be86fa7a58ddf3a41022d2d405
Rikuto <orikutobasuke@gmail.com>
2023-04-11 14:21:09 +0900

    nextscss

--- src/pages/samples/09/components/Header.tsx
+++ src/pages/samples/09/components/Header.tsx
@@ -1,23 +1,52 @@
 import style from './header.module.scss'
 import Link from 'next/link'
-
 export function Header() {
     return (
         <>
             <header className={style.head}>
                 <ul className={style.navbox}>
-                    <Link href={'/'}>
-                        <li>logo</li>
-                    </Link>
-                    <Link href={'/'}>
-                        <li>Food</li>
-                    </Link>
-                    <Link href={'/'}>
-                        <li>Drink</li>
-                    </Link>
-                    <Link href={'/'}>
-                        <li>Outdor</li>
-                    </Link>
+                    <li><Link href={'/'}>logo</Link></li>
+                    <li>
+                        <Link href={'/'}>jfjf</Link>
+                        <ul>
+                            <li>
+                                <Link href={'/'}>aaa</Link>
+                            </li>
+                            <li>
+                                <Link href={'/'}>iiii</Link>
+                            </li>
+                            <li>
+                                <Link href={'/'}>uuuu</Link>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><Link href={'/'}>ldld</Link>
+                        <ul>
+                            <li>
+                                <Link href={'/'}>eeee</Link>
+                            </li>
+                            <li>
+                                <Link href={'/'}>oooo</Link>
+                            </li>
+                            <li>
+                                <Link href={'/'}>kakaka</Link>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <Link href={'/'}>aaaa</Link>
+                        <ul>
+                            <li>
+                                <Link href={'/'}>kikikiki</Link>
+                            </li>
+                            <li>
+                                <Link href={'/'}>kukuku</Link>
+                            </li>
+                            <li>
+                                <Link href={'/'}>keekekeke</Link>
+                            </li>
+                        </ul>
+                    </li>
                 </ul>
             </header>
         </>
