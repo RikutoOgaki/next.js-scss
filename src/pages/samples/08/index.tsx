9db7b774dd5e12d3519ba1d49789ccb50f0350d8
Rikuto <orikutobasuke@gmail.com>
2023-04-11 10:43:12 +0900

    nextscss

--- src/pages/samples/08/index.tsx
+++ src/pages/samples/08/index.tsx
@@ -1,14 +1,30 @@
-import { Header } from './components/Header'
-import { Footer } from './components/Footer'
-import { Navigation } from './components/Navigation'
 import style from './index.module.scss'
+
+export function Header() {
+    return (
+        <>
+            <header className={style.head}>
+                <h1>Hello World</h1>
+            </header>
+        </>
+    )
+}
+
+export function Footer() {
+    return (
+        <>
+            <footer className={style.foot}>
+                <p><small>project &copy; html課題06</small></p>
+            </footer>
+        </>
+    )
+}
 
 export default function Sample08() {
     return (
         <>
             <div className={style.wrap}>
-                <Header />
-                <Footer />
+
             </div>
         </>
     )
