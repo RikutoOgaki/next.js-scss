c28e672fd34771aa62b6dd18f10784d48f1e7119
Rikuto <orikutobasuke@gmail.com>
2023-04-11 10:08:15 +0900

    nextscss

--- src/pages/samples/07/components/Header.tsx
+++ src/pages/samples/07/components/Header.tsx
@@ -1,9 +1,52 @@
-import './header.module.scss'
+import style from './header.module.scss'
+import Link from 'next/link'
+import MediaQuery from 'react-responsive'
+
+export function Headerbox() {
+    return (
+        <div className={style.navbox}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    )
+}
+
+export function Headerbar() {
+    return (
+        <div className={style.listbox}>
+            <ul className={style.nav}>
+                <Link href={'/'}>
+                    <li>logo</li>
+                </Link>
+                <Link href={'/'}>
+                    <li>about</li>
+                </Link>
+                <Link href={'/'}>
+                    <li>skill</li>
+                </Link>
+                <Link href={'/'}>
+                    <li>works</li>
+                </Link>
+                <Link href={'/'}>
+                    <li>example</li>
+                </Link>
+            </ul>
+        </div>
+    )
+}
 
 export function Header() {
     return (
         <>
-
+            <header className={style.head}>
+                <MediaQuery query='(max-width:412px)'>
+                    <Headerbox />
+                </MediaQuery>
+                <MediaQuery query='(min-width:412px)'>
+                    <Headerbar />
+                </MediaQuery>
+            </header>
         </>
     )
 }
--- src/pages/samples/07/index.tsx
+++ src/pages/samples/07/index.tsx
@@ -1,9 +1,11 @@
 import './index.module.scss'
 
+import { Header } from './components/Header'
+
 export default function Sample07() {
     return (
         <>
-
+            <Header />
         </>
     )
 }
