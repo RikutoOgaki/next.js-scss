aa0781a867534595e4517a435be1722713c27c44
Rikuto <orikutobasuke@gmail.com>
2023-04-11 09:22:36 +0900

    nextscss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,9 @@ export default function Index() {
           <Link href={'/samples/01'}><li>sample01</li></Link>
           <Link href={'/samples/02'}><li>sample02</li></Link>
           <Link href={'/samples/03'}><li>sample03</li></Link>
+          <Link href={'/samples/04'}><li>sample04</li></Link>
+          <Link href={'/samples/05'}><li>sample05</li></Link>
+          <Link href={'/samples/06'}><li>sample06</li></Link>
         </ul>
       </div>
     </>
---
+++ src/pages/samples/07/index.tsx
@@ -0,0 +1,5 @@
+export default function Sample07() {
+    return (
+        <></>
+    )
+}
