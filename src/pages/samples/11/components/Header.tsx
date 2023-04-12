3001753242fb067bd1975b8303d00722231b999d
Rikuto <orikutobasuke@gmail.com>
2023-04-12 13:39:45 +0900

    nextscss

--- src/pages/samples/11/components/Header.tsx
+++ src/pages/samples/11/components/Header.tsx
@@ -1,5 +1,21 @@
+import Link from 'next/link'
+import style from './header.module.scss'
+
 export function Header() {
     return (
-        <></>
+        <>
+            <header className={style.head}>
+                <div className={style.headbox}>
+                    <span>☆</span>
+                    <span>SOUND VOLTEX</span>
+                    <ul className={style.nav}>
+                        <li><Link href={'/'}></Link></li>
+                        <li><Link href={'/'}></Link></li>
+                        <li><Link href={'/'}></Link></li>
+                        <li><Link href={'/'}></Link></li>
+                    </ul>
+                </div>
+            </header>
+        </>
     )
 }
