3e8cc242a917679f367b4a6b5bb62d29c4dd1e33
Rikuto <orikutobasuke@gmail.com>
2023-04-12 14:13:43 +0900

    nextscss

---
+++ src/pages/samples/11/components/MainBIjual.tsx
@@ -0,0 +1,5 @@
+export function MainBijual() {
+    return (
+        <></>
+    )
+}
--- src/pages/samples/11/index.tsx
+++ src/pages/samples/11/index.tsx
@@ -1,11 +1,14 @@
 import style from './index.module.scss'
 import { Header } from './components/Header'
+import { MainBijual } from './components/MainBIjual'
+
 
 export default function Sample11() {
     return (
         <>
             <div className={style.wrap}>
                 <Header />
+                <MainBijual />
             </div>
         </>
     )
