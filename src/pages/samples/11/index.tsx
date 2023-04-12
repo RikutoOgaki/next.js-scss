2729013c6ba94e5f4f8fbeb9b380beeb135b508a
Rikuto <orikutobasuke@gmail.com>
2023-04-12 13:30:30 +0900

    nextscss

--- src/pages/samples/11/index.tsx
+++ src/pages/samples/11/index.tsx
@@ -1,14 +1,9 @@
+import { Header } from './components/Header'
+
 export default function Sample11() {
     return (
         <>
-            <div>
-                <ul>
-                    <li><img src="https://artscouncil-niigata.jp/nidf/wp/wp-content/themes/nidf2017/images/common/bg/bg01_01.png" width="2400" height="1420" alt="" /></li>
-                    <li><img src="https://artscouncil-niigata.jp/nidf/wp/wp-content/themes/nidf2017/images/common/bg/bg01_01.png" width="2400" height="1420" alt="" /></li>
-                    <li><img src="https://artscouncil-niigata.jp/nidf/wp/wp-content/themes/nidf2017/images/common/bg/bg01_01.png" width="2400" height="1420" alt="" /></li>
-                    <li><img src="https://artscouncil-niigata.jp/nidf/wp/wp-content/themes/nidf2017/images/common/bg/bg01_01.png" width="2400" height="1420" alt="" /></li>
-                </ul>
-            </div>
+
         </>
     )
 }
