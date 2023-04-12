4e5c8fe677e45cddd71f88727073867013302909
Rikuto <orikutobasuke@gmail.com>
2023-04-12 14:11:10 +0900

    nextscss

--- src/pages/samples/11/components/Header.tsx
+++ src/pages/samples/11/components/Header.tsx
@@ -6,13 +6,16 @@ export function Header() {
         <>
             <header className={style.head}>
                 <div className={style.headbox}>
-                    <span>☆</span>
-                    <span>SOUND VOLTEX</span>
+                    <div className={style.spanbox}>
+                        <span>☆</span>
+                        <span>SOUND VOLTEX</span>
+                    </div>
                     <ul className={style.nav}>
-                        <li><Link href={'/'}></Link></li>
-                        <li><Link href={'/'}></Link></li>
-                        <li><Link href={'/'}></Link></li>
-                        <li><Link href={'/'}></Link></li>
+                        <li><Link href={'/'}>大会概要</Link></li>
+                        <li><Link href={'/'}>ルール説明</Link></li>
+                        <li><Link href={'/'}>ご質問</Link></li>
+                        <li><Link href={'/'}>お問い合わせ</Link></li>
+                        <li><Link href={'/'}>エントリー</Link></li>
                     </ul>
                 </div>
             </header>
--- src/pages/samples/11/index.tsx
+++ src/pages/samples/11/index.tsx
@@ -1,9 +1,12 @@
+import style from './index.module.scss'
 import { Header } from './components/Header'
 
 export default function Sample11() {
     return (
         <>
-
+            <div className={style.wrap}>
+                <Header />
+            </div>
         </>
     )
 }
