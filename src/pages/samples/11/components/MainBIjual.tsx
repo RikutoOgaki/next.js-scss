6e461278b9635f4f99443a76252c19b86ff5b986
Rikuto <orikutobasuke@gmail.com>
2023-04-12 14:58:48 +0900

    nextscss

--- src/pages/samples/11/components/MainBIjual.tsx
+++ src/pages/samples/11/components/MainBIjual.tsx
@@ -4,9 +4,10 @@ export function MainBijual() {
     return (
         <>
             <div className={style.mainImg}>
-                <div className={style.maincontents}></div>
-                <h1 className={style.title}>SOUND VOLTEX</h1>
-                <p className={style.text}>World Chanpion ship</p>
+                <div className={style.maincontents}>
+                    <h1 className={style.title}>SOUND VOLTEX</h1>
+                    <p className={style.text}>World Chanpion ship</p>
+                </div>
             </div>
         </>
     )
