a62a40278f30d7aa808a345a645a95f4cc878d7b
Rikuto <orikutobasuke@gmail.com>
2023-04-14 15:20:33 +0900

    nextscss

---
+++ src/pages/comp/02/components/Skill.tsx
@@ -0,0 +1,71 @@
+import { Box, Flex, Text } from '@chakra-ui/react'
+import {
+    SiHtml5,
+    SiCss3,
+    SiSass,
+    SiJavascript,
+    SiTypescript,
+    SiReact,
+    SiVite,
+    SiNpm,
+    SiChakraui,
+
+    SiNextdotjs,
+    SiVuedotjs,
+    SiPostgresql,
+    SiPhp,
+    SiRuby,
+
+    SiVisualstudiocode,
+    SiFigma,
+    SiAdobeillustrator,
+    SiAdobephotoshop,
+    SiAdobepremierepro,
+    SiAdobeindesign,
+    SiAdobexd,
+    SiNotion,
+    SiSlack
+} from 'react-icons/si'
+
+
+export function Skill() {
+
+    const iconArray = {
+        useicon: [
+            SiHtml5,
+            SiCss3,
+            SiSass,
+            SiJavascript,
+            SiTypescript,
+            SiReact,
+            SiVite,
+            SiNpm,
+            SiChakraui,
+        ],
+        studyicon: [
+            SiNextdotjs,
+            SiVuedotjs,
+            SiPostgresql,
+            SiPhp,
+            SiRuby
+        ],
+        usingicon: [
+            SiVisualstudiocode,
+            SiFigma,
+            SiAdobeillustrator,
+            SiAdobephotoshop,
+            SiAdobepremierepro,
+            SiAdobeindesign,
+            SiAdobexd,
+            SiNotion,
+            SiSlack
+        ]
+    }
+
+
+    return (
+        <>
+
+        </>
+    )
+}
--- src/pages/comp/02/index.tsx
+++ src/pages/comp/02/index.tsx
@@ -2,6 +2,7 @@ import { Box, Flex, Text, Input } from '@chakra-ui/react'
 import { Header } from './components/Header'
 import { Name } from './components/Name'
 import { Profil } from './components/Profil'
+import { Skill } from './components/Skill'
 
 export default function Sample02() {
     return (
